07b035a465feeb35435d8d697c388859c2276f52
Obasa-Oluwatobiloba <ptoblex@gmail.com>
2020-08-07 10:41:47 +0100

    Updates

---
+++ precache-manifest.7ac552ea52a71319830e85baa5f9bdad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c1e542634c50948af5cf283f02b7b2",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "08e819b0a8e0c2a334b3",
+    "url": "/Monster-Rolodex/static/css/main.4a5fac9e.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/Monster-Rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e819b0a8e0c2a334b3",
+    "url": "/Monster-Rolodex/static/js/main.dcc69014.chunk.js"
+  },
+  {
+    "revision": "2b55c592b5ec82aad6ec",
+    "url": "/Monster-Rolodex/static/js/runtime-main.28f6930b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Monster-Rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
